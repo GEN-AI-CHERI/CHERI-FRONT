9171e2491162ae93385ce7e788ed2656fbecc1ea
jimin song <songing01@gmail.com>
2023-07-28 20:58:59 +0900

    ✨ 기능선택 페이지 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import OptionsPage from "./pages/OptionsPage";
 function App() {
   return (
     <Routes>
       <Route path="/main" element={<MainPage />} />
+      <Route path="/options" element={<OptionsPage />} />
     </Routes>
   );
 }
