5b2f2a80db3dee7096f45b1c18bfd5892472f0ae
jimin song <songing01@gmail.com>
2023-07-30 00:10:04 +0900

    ✨ 회원가입,로그인 루트 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import OptionsPage from "./pages/OptionsPage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 function App() {
   return (
     <Routes>
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/main" element={<MainPage />} />
       <Route path="/options" element={<OptionsPage />} />
     </Routes>
