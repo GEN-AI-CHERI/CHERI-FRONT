ad405f587ea537375278d736fdc4d5d55ec1394f
jimin song <songing01@gmail.com>
2023-08-04 00:03:57 +0900

    🐛 axios default instance 오류 수정

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const client = axios.create();
 client.defaults.baseURL = `${process.env.REACT_APP_CHERI_API}`;
-client.defaults.withCredentials = true;
+client.defaults.withCredentials = false;
 
 const token = localStorage.getItem("cheritoken");
 console.log("현재 토큰", token);
