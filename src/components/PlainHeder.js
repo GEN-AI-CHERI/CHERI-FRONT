b18c7a66b8a552e9b887f6c9ea506748d7879d0c
jimin song <songing01@gmail.com>
2023-08-08 01:12:16 +0900

    ✨ 로고 클릭시 메인페이지 라우팅

--- src/components/PlainHeder.js
+++ src/components/PlainHeder.js
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "../assets/common/logo.png";
 import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const PlainHeader = () => {
+  const navigate = useNavigate();
   return (
     <Wrapper>
-      <div className="logo">
+      <div className="logo" onClick={() => navigate("/")}>
         <img src={logo} alt="" width={"38px"} height={"32px"} />
       </div>
     </Wrapper>
@@ -16,8 +18,11 @@ export default PlainHeader;
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 65px;
-  padding-left: 20.5px;
+  height: 70px;
   display: flex;
   align-items: center;
+
+  .logo {
+    margin-left: 19.5px;
+  }
 `;
